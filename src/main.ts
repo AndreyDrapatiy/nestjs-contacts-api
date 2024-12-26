6df1b8704a96f361b333e82219df2ce2db16254f
Andrey Drapatiy <drapatiy92@gmail.com>
2024-12-26 19:56:58 +0200

    Format

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 import * as cookieParser from 'cookie-parser';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    cors({
+      origin: (origin, callback) => {
+        const allowedOrigins = ['http://localhost:3000', 'http://example.com'];
+        if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      credentials: true,
+    }),
+  );
   app.use(cookieParser());
   await app.listen(process.env.PORT ?? 8080);
 }
